b09ba28b21896172a2ff9408f75d78d99db4f0bc
chenmenglong24 <2268234936@qq.com>
2019-06-10 22:48:44 +0800

    vue-components

---
+++ VUE/vue-iview/iview/src/components/common/Alert.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="alert">{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Alter',
+  props: ['msg'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .alert{
+    font-size: 30px;
+    text-align: center;
+    width: 300px;
+    height: 200px;
+    border: 1px solid #aaaaaa;
+    border-radius: 5px;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 999;
+  }
+</style>
