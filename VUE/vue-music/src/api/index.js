f98ecfa9cfed988dd85f7c5f373ef8aeb3de2a8c
chenmenglong24 <2268234936@qq.com>
2019-06-22 18:18:20 +0800

    vue-music

--- VUE/vue-music/src/api/index.js
+++ VUE/vue-music/src/api/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import axios from 'axios'
-
 const vue = new Vue()
 
 // axios配置
@@ -9,7 +8,7 @@ axios.defaults.baseURL = 'http://localhost:3000'
 
 // 返回状态判断
 axios.interceptors.response.use((res) => {
-  if (res.data.code !==200) {
+  if (res.data.code !== 200) {
     vue.$toast('网络异常')
     vue.$hideLoading()
     return Promise.reject(res)
@@ -23,16 +22,17 @@ axios.interceptors.response.use((res) => {
 
 export function fetchGet(url, param) {
   return new Promise((resolve, reject) => {
-    axios
-      .get(url, {param: param})
-      .then(response => {
-        resolve(response.data)
-      }, err => {
-        reject(err)
-      })
-      .catch((error) => {
-        reject(err)
-      })
+    axios.get(url, {
+      params: param
+    })
+    .then(response => {
+      resolve(response.data)
+    }, err => {
+      reject(err)
+    })
+    .catch((error) => {
+      reject(error)
+    })
   })
 }
 
@@ -45,12 +45,12 @@ export default {
   BannerList() {
     return fetchGet('/banner')
   },
-  // 歌单
+  //歌单
   DiscLists(params) {
     return fetchGet('/top/playlist', params)
   },
   // 歌单详情
   SongList(params) {
-    return fetchGet('./playlist/detail', params)
+    return fetchGet('/playlist/detail', params)
   }
 }
