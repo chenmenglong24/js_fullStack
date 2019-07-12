0e5368c2791a3a4f2242030be75893a8a96c62b6
chenmenglong24 <2268234936@qq.com>
2019-07-12 16:33:14 +0800

    中序遍历

--- leetcode/sort/bubble_sort.js
+++ leetcode/sort/bubble_sort.js
@@ -1,19 +1,16 @@
 function bubbleSort (arr) {
-  var len = arr.length;
-  var minIndex, temp;
-  for (var i = 0; i < len -1; i++) {
-    minIndex = i;
-    for (var j = i + 1; j < len; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
+  let len = arr.length;
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
       }
     }
-    temp = arr[i];
-    arr[i] = arr[minIndex];
-    arr[minIndex] = temp;
   }
   return arr;
 }
-// 每次让一个数排好 两两交换位置
-const arr = [8, 2 ,5, 9, 7]
-console.log(bubbleSort(arr))
+
+const arr = [5,8,4,6,3,9,7,2,1];
+console.log(bubbleSort(arr)) 
