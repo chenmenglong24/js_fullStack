e2418f11575c831a5efce260f88a366dad854460
chenmenglong24 <2268234936@qq.com>
2019-07-15 20:47:05 +0800

    beer

---
+++ react/beer/src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Loader = (props) => (
+  <div className="loader">
+    <img src="/image/ball.svg" alt="loading"/>
+    <h2>{props.message}</h2>
+  </div>
+)
+Loader.propTypes = {
+  message: PropTypes.string 
+}
+
+export default Loader;
---
+++ react/beer/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Main from './components/Main';
+import Single from './components/Single';
+
+
+const Root = function () {
+  return (
+    <BrowserRouter>
+    <Route path='/' component={Main}/>
+    <Route path='/search/:searchTerm' component={Main}/>
+    <Route path='/beer/:beerId/:beerSlug' component={Single}/>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.getElementById('root'));
