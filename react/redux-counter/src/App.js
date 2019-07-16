f677243d92847cc1552da38723eb485b0943aa37
chenmenglong24 <2268234936@qq.com>
2019-07-16 21:17:01 +0800

    ts

--- react/redux-counter/src/App.js
+++ react/redux-counter/src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Count extends React.Component {
+  state = {  }
+  render() { 
+    console.log(this.props);
+    const { count, films } = this.props
+    return (  
+      <div>
+        count: {count}
+        <button onClick={this.handleIn}>+</button>
+        <button onClick={this.handleDe}>-</button>
+        <button onClick={this.handleAddFilm}>添加电影</button>
+        {
+          films.map((film, i) => {
+            return (
+              <li key={i}>{film.name}</li>
+            )
+          })
+        }
+      </div>
+    );
+  }
+  handleIn = () => {
+    // store.dispatch({type: 'INCREMENT'})
+    this.props.incerment();
+  }
+  handleDe = () => {
+    // store.dispatch({type: 'DECREMENT'})
+    this.props.decrement();
+  }
+  handleAddFilm = () => {
+  //   store.dispatch({
+  //     type: 'ADD_FILM', 
+  //     film: {name: '黑暗骑士'}
+  //   })
+  this.props.addFilm();
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+    films: state.films
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    foo: () => {console.log('foo')},
+    incerment: () => { dispatch({ type: 'INCREMENT' }) },
+    decrement: () => { dispatch({ type: 'DECREMENT' }) },
+    addFilm: () => { dispatch({ type: 'ADD_FILM', film: {name: "黑暗骑士"} }) }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Count);
