3e43f640aec6eecee8697c6862b7f2d41754c9ca
chenmenglong24 <2268234936@qq.com>
2019-08-10 17:21:37 +0800

    vue语法

---
+++ VUE/element/src/components/ElButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button class="el-button" :disabled="buttonDisabled ? 'disabled' : ''" 
+            :class="[type ? 'el-button--' + type : '', 
+                     size ? 'el-button--' + size : '',
+                     {'is-disabled': buttonDisabled}
+                     ]"
+      @click="handleClick">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      string: String,
+      default: 'default'
+    },
+    size: String,
+    disabled: Boolean
+  },
+  computed: {
+    buttonDisabled () {
+      return this.disabled
+    }
+  },
+  methods: {
+    handleClick (evt) {
+      this.$emit('clickBtn', evt);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
