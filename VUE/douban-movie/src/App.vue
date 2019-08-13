3db10c14b950da42c51c505f0475759a8b5cbb39
chenmenglong24 <2268234936@qq.com>
2019-08-13 23:22:25 +0800

    面试

--- VUE/douban-movie/src/App.vue
+++ VUE/douban-movie/src/App.vue
@@ -3,9 +3,9 @@
     <!-- 头部 -->
     <TheHeader></TheHeader>
     <div class="content">
-      <keep-alive>
+      <!-- <keep-alive> -->
         <router-view/>
-      </keep-alive>
+      <!-- </keep-alive> -->
     </div>
   </div>
 </template>
